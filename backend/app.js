f52a83bfeee0a77f33bf5035400d8f374cbdd1db
YaroslavGolovatyy <yaroslavgolovatyy@gmail.com>
2023-07-12 14:07:54 +0300

    created logic for login-signup and started backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+mongoose.connect('mongodb://localhost:27017/wordsdatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log('Connected to mongodb://localhost:27017/wordsdatabase')
+})
+.catch((error)=>{
+    console.log('Failed to connect to MongoDB', error);
+});
+
 app.use((req, res, next)=>{
     console.log('the request is finished')
     next();
