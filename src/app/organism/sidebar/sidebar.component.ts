7175b25a90d0de366eca722821402442f74401bd
YaroslavGolovatyy <yaroslavgolovatyy@gmail.com>
2023-08-11 12:41:59 +0300

    making better view

--- src/app/organism/sidebar/sidebar.component.ts
+++ src/app/organism/sidebar/sidebar.component.ts
@@ -6,51 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  menus = [
-    {
-      name: 'Vocabulary',
-      buttons: [
-        {
-          buttonName:'Home',
-          buttonIcon: '../../../../assets/sidebar/Home-icon.svg',
-          routerHref: '/'
-        }, 
-        {
-          buttonName: 'Categories',
-          buttonIcon: '../../../../assets/sidebar/categories.svg',
-          routerHref: '/'
-        },
-        {
-          buttonName: 'Donate term',
-          buttonIcon: '../../../../assets/sidebar/Add term-icon.svg',
-          routerHref: '/'
-        }
-      ]
-    },
-    {
-      name: 'Learning',
-      buttons: [       
-        {
-          buttonName: 'Rules',
-          buttonIcon: '../../../../assets/sidebar/Rules-icon.svg',
-          routerHref: '/about'
-        },
-        {
-          buttonName: 'Exercises',
-          buttonIcon: '../../../../assets/sidebar/Exercise-icon.svg',
-          routerHref: '/about'
-        },
-        {
-          buttonName: 'About us',
-          buttonIcon: '../../../../assets/sidebar/About us-icon.svg',
-          routerHref: '/about'
-        },
 
-      ]
-    },
-  ]
   idiom: string = 'walk on air'
-
-
 
 }
