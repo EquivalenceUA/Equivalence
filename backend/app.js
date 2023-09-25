cb673fc248197957aa85b30546dbca24d3dc8fa7
YaroslavHolovatyi <yaroslavgolovatyy@gmail.com>
2023-09-25 19:16:46 +0300

    some changes for backend to work

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/user-routes");
 const cors = require('cors'); 
+const port = process.env.PORT || 3000;
 
 
 const corsOptions = {
   origin: 'http://localhost:4200'
   };
+
 const app = express();
+
+
 app.use(express.json());
 app.use(cors(corsOptions))
 app.use("/api/users", router);
+app.get('/', (req, res) => res.json('Equivalence API working'))
 
 
 mongoose
     .connect(
         "mongodb+srv://admin:Barabolya987^@cluster0.i2iiqvu.mongodb.net/?retryWrites=true&w=majority"
     )
-    .then(()=>app.listen(3000))
+    .then(()=>app.listen(port))
     .then(()=>
-    console.log("Connected to Database and listening to Localhost 5000")
+    console.log("Connected to Database and listening to ", port )
     )
     .catch((err)=> console.error(err));
